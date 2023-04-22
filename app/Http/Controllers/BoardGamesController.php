6aa971c88e4a8cfc994f1048b4f14cd8219c8ebf
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-22 17:59:03 +0700

    Add files via upload

--- app/Http/Controllers/BoardGamesController.php
+++ app/Http/Controllers/BoardGamesController.php
@@ -2,100 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BoardGames;
-use App\Models\Category;
+use App\Models\Boardgame;
 use Illuminate\Http\Request;
 
-class BoardGamesController extends Controller
+class GameController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
-        $boardgames = boardgames::all();
-        // dd($boardgamess);
-        return view('boardgame.index', ['boardgames' => $boardgames]);
+        $games = Boardgame::all();
+        return view('games.index', compact('games'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        $boardgames = boardgames::all();
-        return view('boardgames.create', ['boardgames' => $boardgames]);
-        //
+        return view('games.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        $boardgame = new BoardGames();
-        $boardgame->ID = $request->input('ID');
-        $boardgame->Name = $request->input('Name');
-        $boardgame->Category = $request->input('Category');
-        $boardgame->Description = $request->input('Description');
-        $boardgame->Quantity = $request->input('Quantity');
-        $boardgame->Price = $request->input('Price');
-        $boardgame->save();
-        $boardgame->category()->attach($request->input('category'));
-        return redirect()->route('boardgames.index');
-
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-        $boardgame = BoardGames::find($id);
-        return view('boardgames.detail', ['boargames' => $boardgame]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-        $boardgame = BoardGames::find($id);
-        $categories = Category::all();
-        return view('boardgames.edit', [
-            'boardgames' => $boardgame, 
-            'categories' => $categories
+        $game = new Boardgame([
+            'name' => $request->get('name'),
+            'description' => $request->get('description')
         ]);
+        $game->save();
+        return redirect('/games')->with('success', 'Game has been added');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function edit($id)
     {
-        //
-        $boardgame = BoardGames::find($id);
-        $boardgame->boardgames_name = $request->boardgames_name;
-        $boardgame->boardgames_description = $request->boardgames_description;
-        $boardgame->boardgames_quantity = $request->boardgames_quantity;
-        $boardgame->boardgames_price = $request->boardgames_price;
-        $boardgame->category_id = $request->category_id;
-        $boardgame->save();
-        return redirect('/boardgames')->with('success', 'Boardgame updated successfully.');
+        $game = Boardgame::find($id);
+        return view('games.edit', compact('game'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function update(Request $request, $id)
     {
-        //
-        $boardgame = BoardGames::find($id);
-        $boardgame->delete();
-        return redirect('/boardgames')->with('success', 'Boardgame deleted successfully.');
+        $game = Boardgame::find($id);
+        $game->name = $request->get('name');
+        $game->description = $request->get('description');
+        $game->save();
+        return redirect('/games')->with('success', 'Game has been updated');
+    }
+
+    public function destroy($id)
+    {
+        $game = Boardgame::find($id);
+        $game->delete();
+        return redirect('/games')->with('success', 'Game has been deleted');
     }
 }

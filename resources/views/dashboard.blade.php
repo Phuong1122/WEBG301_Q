c9e381f367202460e733a2a3310de1b780a4e2e7
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-22 17:26:44 +0700

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Trang quản trị</title>
+  </head>
+  <body>
+    <h1>Trang quản trị</h1>
+    <p>Xin chào, {{ Auth::user()->name }}</p>
+    <nav>
+      <ul>
+        <li><a href="{{ route('games.index') }}">Danh sách boardgame</a></li>
+        <li><a href="{{ route('games.create') }}">Thêm boardgame mới</a></li>
+        <li><a href="{{ route('logout') }}">Đăng xuất</a></li>
+      </ul>
+    </nav>
+    <hr>
+    @yield('content')
+  </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Đăng nhập</title>
+  </head>
+  <body>
+    <h1>Đăng nhập</h1>
+    @if ($errors->any())
+      <div>
+        <strong>Lỗi:</strong>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+    <form method="POST" action="{{ route('login') }}">
+      @csrf
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required autofocus>
+      </div>
+      <div>
+        <label for="password">Mật khẩu:</label>
+        <input type="password" name="password" id="password" required>
+      </div>
+      <div>
+        <input type="checkbox" name="remember" id="remember">
+        <label for="remember">Ghi nhớ đăng nhập</label>
+      </div>
+      <div>
+        <button type="submit">Đăng nhập</button>
+      </div>
+    </form>
+  </body>
+</html>

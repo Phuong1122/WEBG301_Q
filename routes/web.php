42e9153a8b05aa481125ea65b3a7de487d145784
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-23 01:27:36 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\GameController;
+use App\Http\Controllers\BoardGamesController;
 
-Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
@@ -15,10 +13,10 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
 
-    Route::get('/games', [GameController::class, 'index']);
-    Route::get('/games/create', [GameController::class, 'create']);
-    Route::post('/games', [GameController::class, 'store']);
-    Route::get('/games/{id}/edit', [GameController::class, 'edit']);
-    Route::put('/games/{id}', [GameController::class, 'update']);
-    Route::delete('/games/{id}', [GameController::class, 'destroy']);
+    Route::get('/games', [BoardGameController::class, 'index']);
+    Route::get('/games/create', [BoardGameController::class, 'create']);
+    Route::post('/games', [BoardGameController::class, 'store']);
+    Route::get('/games/{id}/edit', [BoardGameController::class, 'edit']);
+    Route::put('/games/{id}', [BoardGameController::class, 'update']);
+    Route::delete('/games/{id}', [BoardGameController::class, 'destroy']);
 });

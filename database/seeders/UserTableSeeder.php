4a002601dc8ad07cde559c834276ddd62152b4c3
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-27 01:59:55 +0700

    Add files via upload

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $data = [
+            [
+                'email'=>'anhemtaopro@gmail.com',
+                'password'=>bcrypt('123456'),
+                'level'=>1
+            ],
+            [
+                'email'=>'admin@gmail.com',
+                'password'=>bcrypt('123456'),
+                'level'=>1
+            ],
+        ];
+        DB::table('vp_users')->insert($data);
+    }
+}

213bff6b37106e8a904abd8561663ddc9c0f7764
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-27 02:01:25 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BoardGamesController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SalesController;
-use App\Http\Controllers\CustomersController;
-use App\Http\Controllers\CartsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return view('front.index');
+    return view('welcome');
 });
 
-Route::get('/shop/product/{$id}', function () {
-    return view('front.show');
+Route::group(['namespace'=>'Admin'],function(){
+    Route::group(['prefix'=>'login'],function(){
+        Route::get('/','LoginController@getLogin');
+    });
 });

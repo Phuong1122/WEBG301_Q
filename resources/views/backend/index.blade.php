72236ad6d6ed21572a4702b65d0735068324cc6b
Phuong1122 <128888119+Phuong1122@users.noreply.github.com>
2023-04-27 01:58:30 +0700

    Add files via upload

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,3 +1,6 @@
+@extends('backend.master')
+@section('title','Trang chủ quản trị')
+@section('main')
 	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">		
 		<div class="row">
 			<div class="col-lg-12">
@@ -70,32 +73,5 @@
 				</div>
 			</div><!--/.col-->
 		</div><!--/.row-->
-	</div>	<!--/.main-->
-		  
-
-	<script src="js/jquery-1.11.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/chart.min.js"></script>
-	<script src="js/chart-data.js"></script>
-	<script src="js/easypiechart.js"></script>
-	<script src="js/easypiechart-data.js"></script>
-	<script src="js/bootstrap-datepicker.js"></script>
-	<script>
-		$('#calendar').datepicker({
-		});
-		!function ($) {
-		    $(document).on("click","ul.nav li.parent > a > span.icon", function(){          
-		        $(this).find('em:first').toggleClass("glyphicon-minus");      
-		    }); 
-		    $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
-		}(window.jQuery);
-		$(window).on('resize', function () {
-		  if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
-		})
-		$(window).on('resize', function () {
-		  if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
-		})
-	</script>	
-</body>
-
-</html>
+	</div>	
+@stop

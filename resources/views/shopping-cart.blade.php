5977c38abd3f56c486a86f49f80434e34b08a36d
Nguyễn Thành Long <128024085+ThanhhLonggg@users.noreply.github.com>
2023-04-27 16:02:36 +0700

    Add files via upload

--- resources/views/shopping-cart.blade.php
+++ resources/views/shopping-cart.blade.php
@@ -185,6 +185,111 @@
     </header>
     <!-- Header Section End -->
 
+    <!-- Breadcrumb Section Begin -->
+    <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb-text">
+                        <a href="index.blade.php"><i class="fa fa-home"></i>Home</a>
+                        <a href="shop.blade.php">Shop</a>
+                        <span>Shopping Cart</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Shopping Cart Section Begin -->
+    <div class="shopping-cart spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="cart-table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Image</th>
+                                    <th class="p-name">Product Name</th>
+                                    <th>Price</th>
+                                    <th>Quantity</th>
+                                    <th>Total</th>
+                                    <th><i class="ti-close"></i></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="cart-pic first-row"><img src="front/img/cart-page/product-1.jpg" alt=""></td>
+                                    <td class="cart-title first-row"></td>
+                                    <h5> Pure Pineapple</h5>
+                                    <td class="p=price first-row">1tr$</td>
+                                    <td class="p-price first-row">
+                                        <div class="quantity">
+                                            <div class="pro-qty">
+                                                <input type="text" value="1">
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="total-price frist-row">1tr$</td>
+                                    <td class="close-td first-row"><i class="ti-close"></i></td>
+                                </tr>
+                                <tr>
+                                    <td class="cart-pic "><img src="front/img/cart-page/product-1.jpg" alt=""></td>
+                                    <td class="cart-title "></td>
+                                    <h5> Pure Pineapple</h5>
+                                    <td class="p=price ">1tr$</td>
+                                    <td class="p-price ">
+                                        <div class="quantity">
+                                            <div class="pro-qty">
+                                                <input type="text" value="1">
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="total-price ">1tr$</td>
+                                    <td class="close-td" ><i class="ti-close"></i></td>
+                                </tr>
+                                <tr>
+                                    <td class="cart-pic "><img src="front/img/cart-page/product-1.jpg" alt=""></td>
+                                    <td class="cart-title "></td>
+                                    <h5> Pure Pineapple</h5>
+                                    <td class="p=price ">1tr$</td>
+                                    <td class="p-price ">
+                                        <div class="quantity">
+                                            <div class="pro-qty">
+                                                <input type="text" value="1">
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="total-price ">1tr$</td>
+                                    <td class="close-td "><i class="ti-close"></i></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="cart-buttons">
+                                <a href="#" class="primary-btn continue-shop">Continue Shopping</a>
+                                <a href="#" class="primary-btn up-cart">Update cart</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 offset-lg-4">
+                            <div class="proceed-checkout">
+                                <ul>
+                                    <li class="subtotal">Subtotal <span>360tr$</span></li>
+                                    <li class="cart-total">Total <span>240tr$</span></li>
+                                </ul>
+                                <a href="check-out.blade.php" class="proceed-btn">Proced to Check Out</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Shopping Cart Section End -->
+
     <!-- Partner Logo Section Begin -->
     <div class="partner-logo">
         <div class="container">

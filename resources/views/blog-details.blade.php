403f7e62f51502d7611d7067414213c9ae236bb4
Nguyễn Thành Long <128024085+ThanhhLonggg@users.noreply.github.com>
2023-04-27 12:58:34 +0700

    Add files via upload

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -13,15 +13,15 @@
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="front/css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="front/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="front/css/themify-icons.css" type="text/css">
-    <link rel="stylesheet" href="front/css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="front/css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="front/css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="front/css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="front/css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="front/css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/themify-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 
 <body>
@@ -296,16 +296,16 @@
     <!-- Footer Section End -->
 
     <!-- Js Plugins -->
-    <script src="front/js/jquery-3.3.1.min.js"></script>
-    <script src="front/js/bootstrap.min.js"></script>
-    <script src="front/js/jquery-ui.min.js"></script>
-    <script src="front/js/jquery.countdown.min.js"></script>
-    <script src="front/js/jquery.nice-select.min.js"></script>
-    <script src="front/js/jquery.zoom.min.js"></script>
-    <script src="front/js/jquery.dd.min.js"></script>
-    <script src="front/js/jquery.slicknav.js"></script>
-    <script src="front/js/owl.carousel.min.js"></script>
-    <script src="front/js/main.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery.zoom.min.js"></script>
+    <script src="js/jquery.dd.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>

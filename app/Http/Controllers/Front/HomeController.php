ac621647a70046b0a17f6f6a6695a51b5ab9a10d
QuyDoanVo <128034930+QuyDoanVo@users.noreply.github.com>
2023-04-27 14:59:02 +0700

    Add files via upload

---
+++ app/Http/Controllers/Front/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        $manProducts = Product::where('featured', true)->where('product_category_id',1)->get();
+        $womenProducts = Product::where('featured', true)->where('product_category_id',2)->get();
+        
+
+
+        return view('front.index', compact('manProducts', 'womenProducts'));
+    }
+}
